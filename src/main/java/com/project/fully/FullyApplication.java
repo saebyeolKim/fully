10c48a1f0e515c59ba55532d665bff57115ea184
saebyeolKim <toquf2611@naver.com>
2025-01-29 13:04:30 +0900

    Initail commit

---
+++ src/main/java/com/project/fully/FullyApplication.java
@@ -0,0 +1,13 @@
+package com.project.fully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullyApplication.class, args);
+	}
+
+}

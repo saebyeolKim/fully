99660412e17a83e071ef6e8eba2901e6b075056a
saebyeolKim <toquf2611@naver.com>
2025-01-29 13:24:06 +0900

    feat: 기본 설정

---
+++ src/main/java/com/project/fully/entity/User.java
@@ -0,0 +1,21 @@
+package com.project.fully.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "Member")
+@Getter
+public class User {
+
+    @Id
+    @Column(name = "memberId", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
